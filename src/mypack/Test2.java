fd50b53d8f023db99020a4ba049fbeceee6817e8
priya528 <65577501+priya528@users.noreply.github.com>
2020-09-10 18:37:18 -0500

    Update Test2.java

--- src/mypack/Test2.java
+++ src/mypack/Test2.java
@@ -7,7 +7,7 @@ public class Test2
 	public static void main(String[] args) 
 	
 	{
-		System.out.println("thank u rohan for github teaching");
+		System.out.println("thank u rohan very much for github teaching");
 
 	}
 
